f7ba61ccec11c52efcd8f09ee7b704c3f8eeb0ce
abishajohn <abishajohn15@gmail.com>
2022-03-12 13:57:56 +0530

    Updated my code

---
+++ Calculator.java
@@ -0,0 +1,20 @@
+public class Calculator
+{
+public static void main(String[] args)
+{
+Calculator calc= new Calculator();
+	
+	calc.subtract();//method calling statement
+   
+}
+	public void add()
+	{
+		System.out.println(5+10);//15
+	
+}
+	public void subtract()
+	{
+		System.out.println(10-5);//5
+	
+}
+}
