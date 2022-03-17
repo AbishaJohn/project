220adf258b3d31656b8ce938f133ac83efad86a8
AbishaJohn <99978293+AbishaJohn@users.noreply.github.com>
2022-03-17 18:30:19 +0530

    Add files via upload

---
+++ SmartPhone.java
@@ -0,0 +1,15 @@
+public abstract class SmartPhone
+{
+	public SmartPhone()
+{
+System.out.println("Smartphone under development");
+}
+
+public abstract int call(int second);
+public abstract void sendMessage();
+public abstract void receiveCall();
+public void browse()
+{
+System.out.println("SmartPhone browsing");
+	}
+				   }
