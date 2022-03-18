f20a7fa2818cab55bc104ad182aa7efa1f0c54b7
AbishaJohn <99978293+AbishaJohn@users.noreply.github.com>
2022-03-18 15:12:35 +0530

    Add files via upload

---
+++ India.java
@@ -0,0 +1,12 @@
+public abstract class India
+{
+String capital="New Delhi";
+public India(String PrimeMinister)
+{
+System.out.println("Our Prime Minister is"+ PrimeMinister);
+}
+public abstract void speakLanguage();
+public abstract void eat();
+public abstract void dress();
+
+}
