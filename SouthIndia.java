180fb64b85a406f5d70c39809739656fd42fe67e
AbishaJohn <99978293+AbishaJohn@users.noreply.github.com>
2022-03-18 15:11:41 +0530

    Add files via upload

---
+++ SouthIndia.java
@@ -0,0 +1,18 @@
+public abstract class SouthIndia extends India
+{
+public SouthIndia()
+{
+super("PM");
+System.out.println("SouthIndia");
+}
+public void cultivate()
+{
+System.out.println("Rice and Sugar cane cultivation");
+}
+
+public void livingStyle()
+{
+System.out.println("Above Average development");
+}
+
+}
